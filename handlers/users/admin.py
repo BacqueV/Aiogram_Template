e3eb28565b8fc85099845f07d7a8669a50c050a8
BacqueV <ahmedovbahtiar55@gmail.com>
2023-04-26 00:24:28 +0500

    Update admin.py

--- handlers/users/admin.py
+++ handlers/users/admin.py
@@ -4,6 +4,7 @@ from data.config import ADMINS
 from loader import dp, db, bot
 import pandas as pd
 
+
 @dp.message_handler(text="/allusers", user_id=ADMINS)
 async def get_all_users(message: types.Message):
     users = await db.select_all_users()
@@ -22,18 +23,19 @@ async def get_all_users(message: types.Message):
         for x in range(0, len(df), 50):
             await bot.send_message(message.chat.id, df[x:x + 50])
     else:
-       await bot.send_message(message.chat.id, df)
-       
+        await bot.send_message(message.chat.id, df)
 
-@dp.message_handler(text="/reklama", user_id=ADMINS)
+
+@dp.message_handler(text="/advert", user_id=ADMINS)
 async def send_ad_to_all(message: types.Message):
     users = await db.select_all_users()
     for user in users:
         user_id = user[-1]
-        await bot.send_message(chat_id=user_id, text="@BekoDev kanaliga obuna bo'ling!")
+        await bot.send_message(chat_id=user_id, text="@aiogram - join us!")
         await asyncio.sleep(0.05)
+
 
 @dp.message_handler(text="/cleandb", user_id=ADMINS)
 async def get_all_users(message: types.Message):
     await db.delete_users()
-    await message.answer("Baza tozalandi!")
+    await message.answer("Database is cleared!")
